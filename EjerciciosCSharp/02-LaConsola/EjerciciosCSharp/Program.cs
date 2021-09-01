e2acdbbadbef13bb7ebc0e716297ebdeb85663e1
Florencia Antognini <florenciaantognini@gmail.com>
2021-09-01 00:19:11 -0300

    Initial commit

---
+++ EjerciciosCSharp/02-LaConsola/EjerciciosCSharp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "";
+
+            Console.WriteLine("Por favor ingrese un texto");
+
+            texto = Console.ReadLine();
+            if (texto == "")
+                {
+                Console.WriteLine("Debe escribir algo...!!");
+            } 
+
+            Console.WriteLine("Usted acaba de escribir: " + texto);
+
+            Console.ReadKey();
+            
+           
+        }
+    }
+}
